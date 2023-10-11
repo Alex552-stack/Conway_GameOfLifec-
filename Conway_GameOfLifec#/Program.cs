e2b8d74b996fad3430ae8ed9b2286ccef4c9bd38
Alex <Alex@ALEXSLITTLEWARM>
2023-10-11 11:29:56 +0300

    Add project files.

---
+++ Conway_GameOfLifec#/Program.cs
@@ -0,0 +1,50 @@
+﻿using Conway_GameOfLifec_.Clases;
+
+namespace Conway_GameOfLifec_
+{
+    internal class Program
+    {
+        static int? input()
+        {
+            string str = Console.ReadLine();
+
+            try
+            {
+                int nr = int.Parse(str);
+                return nr;
+            }
+            catch
+            {
+                Console.WriteLine("invalid input");
+                return null;
+            }
+
+        }
+
+        static int AskInput(string message)
+        {
+            int? var = null;
+            Console.WriteLine(message);
+            while (var == null)
+            {
+                var = input();
+            }
+            return (int)var;
+        }
+
+        static void Main(string[] args)
+        {
+            int lines = AskInput("The desired number of lines:");
+            int rows = AskInput("The desired number of rows");
+            
+            GameBord gameBord = new GameBord(lines, rows);
+
+            int chance = AskInput("The desired percentage of live cells(everything above 100 will fill the wrole bord)");
+
+            gameBord.Generate(chance);
+            gameBord.PrintPixels();
+            while (true)
+                gameBord.Simulate(); 
+        }
+    }
+}
